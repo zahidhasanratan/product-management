41690f18ef1ed71fe6d4865587b9cf59b2d7b412
zahid <zahid.hasan6@gmail.com>
2025-04-25 11:42:47 +0600

    redme added

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\API\ProductController;
 
-//Route::get('/user', function (Request $request) {
-//    return $request->user();
-//})->middleware('auth:sanctum');
+
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::post('/products', [ProductController::class, 'store']);
